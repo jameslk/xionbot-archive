5117eb1fd7bccaa1507402eb2b632b274a89935f
James Koshigoe <james@jameskoshigoe.com>
2005-03-26 12:06:24 +0000

    *** empty log message ***

--- src/irc_user_old.h
+++ src/irc_user_old.h
@@ -1,7 +1,8 @@
 /*
-COPYRIGHT limefire/shocktart/SeLfkiLL/Jaleko/f0rk/James/etc. 2004
-www.jaleko.tk
-Licensed under the GPL license.
+COPYRIGHT XionBot Developement Team 2003 - 2005
+http://xionbot.sourceforge.net
+Licensed under the (Open Source) GNU Public License (GPL).
+A copy of the GPL license should have been supplied.
 http://www.gnu.org/licenses/gpl.txt
 */
 
